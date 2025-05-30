26c0e81c794940c5b438a15cf8ad3ceb07ef71c1
Roy Onrust <roy.onrust@gmail.com>
2025-05-30 12:20:25 +0200

    file setup

---
+++ js/script.js
@@ -0,0 +1,14 @@
+﻿const lines = ["Game Developer", "3D Artist", "Programmer", "Game Designer", "Generalist"];
+let index = 0;
+
+const textEl = document.getElementById("rotating-text");
+
+setInterval(() => {
+    index = (index + 1) % lines.length;
+    textEl.classList.add("opacity-0");
+
+    setTimeout(() => {
+        textEl.textContent = lines[index];
+        textEl.classList.remove("opacity-0");
+    }, 400);//Duration
+}, 3000);
